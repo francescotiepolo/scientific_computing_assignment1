0dd95b643147939ab4eb44e252f4149312fbab7a
francescotiepolo <francescotiepolo@icloud.com>
2025-02-08 16:41:43 +0100

    Added markdowns

--- src/solve_wave_eq_func.py
+++ src/solve_wave_eq_func.py
@@ -1,9 +1,18 @@
 import numpy as np
 from numba import njit
 
-# Solve the wave equation using the finite difference method taking as input Psi(x, t=0)
-@njit
+@njit # Numba implementation
 def solve_wave_eq(psi_0, x, t_steps, N, c, dt, dx):
+    '''
+    Solve the wave equation using the finite difference method
+    - psi_0: psi(x, t=0) depending on the initial conidtion
+    - x: spatial step
+    - t_steps: number of time steps
+    - N: number of spatial steps
+    - c: constant
+    - dt: time step size
+    - dx: spatial step size
+    '''
     psi_prev = np.zeros_like(x) # Psi(x, t - dt), initially 0
     psi_curr = psi_0.copy() # Psi(x, t), initially psi_0
     psi_next = np.zeros_like(x) # Preallocate Psi(x, t + dt)
@@ -16,7 +25,7 @@ def solve_wave_eq(psi_0, x, t_steps, N, c, dt, dx):
 
     for n in range(t_steps): # Loop over every timestep
 
-        # Update Psi(x, t + dt) using the finite difference method
+        # Update Psi(x, t + dt) using the finite difference method (excluding boundries)
         psi_next[1:N] = (
             2 * psi_curr[1:N] - psi_prev[1:N] +
             coeff * (psi_curr[2:N+1] - 2 * psi_curr[1:N] + psi_curr[0:N-1])
